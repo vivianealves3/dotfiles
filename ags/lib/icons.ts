2bece1dc6a927f3a9563f55e0dad3d332663ae8c
Aylur <demstof@gmail.com>
2024-03-27 15:54:44 +0100

    ags: colored fallback executable icons

--- ags/lib/icons.ts
+++ ags/lib/icons.ts
@@ -56,12 +56,11 @@ export default {
       medium: "microphone-sensitivity-medium-symbolic",
       high: "microphone-sensitivity-high-symbolic",
     },
-    volume: {
-      muted: "audio-volume-muted-symbolic",
-      low: "audio-volume-low-symbolic",
-      medium: "audio-volume-medium-symbolic",
-      high: "audio-volume-high-symbolic",
-      overamplified: "audio-volume-overamplified-symbolic",
+    fallback: {
+      executable: "application-x-executable",
+      notification: "dialog-information-symbolic",
+      video: "video-x-generic-symbolic",
+      audio: "audio-x-generic-symbolic",
     },
     type: {
       headset: "audio-headphones-symbolic",
